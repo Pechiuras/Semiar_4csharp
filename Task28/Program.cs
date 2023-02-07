714d1b5cb0c2414733e6a23f79a418703a45874b
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-07 12:11:50 +0500

    task28

---
+++ Task28/Program.cs
@@ -0,0 +1,11 @@
+﻿//Task 28 Напишите программу, которая принимает на вход число и выдает произведение чисел от одного до n.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), F = 1, i = 2;
+while (i <= n)
+{
+    F = F * i;
+    i++; //i = i + 1
+   
+}
+Console.WriteLine(F);
