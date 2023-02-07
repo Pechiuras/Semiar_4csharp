2c45704f50aae1cd8b9db6361d8b64d98096a1e4
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-07 12:09:57 +0500

    String

---
+++ String/Program.cs
@@ -0,0 +1,15 @@
+﻿//String(char)
+
+// string? stroka = Console.ReadLine();
+
+// Console.WriteLine(Convert.ToInt32(stroka[0].ToString()) * 10);
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(-10, 11); //[-10, 10]
+
+Console.WriteLine($"Результат: [{string.Join(" , ", array)}]");    
+
