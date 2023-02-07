df4f591baefaea284db055797089284b3d408597
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-07 12:09:29 +0500

    Func

---
+++ Function/Program.cs
@@ -0,0 +1,22 @@
+﻿// Function (void)
+// void f(int[] n)
+// {
+//    n[0] = n[0] + 5; 
+// }
+
+// int[] n = {1, 2, 3, 4, 5};
+// f(n);
+// Console.WriteLine(string.Join(" ", n));
+
+//Напишите программу, которая принимает 2 числа и выводит наибольшее
+
+int f(int a, int b)
+{
+    if (a > b)
+       return a;
+    return b;   
+}
+
+
+int a = 10, b = 15;
+Console.WriteLine(f(a, b));
